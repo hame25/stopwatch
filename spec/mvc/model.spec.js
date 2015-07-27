3b615761be5325d9c8fd05bceb0a5137fe6bf2b9
hame25 <hamishhamer112@hotmail.com>
2015-07-27 22:17:01 +0100

    Added model specs

--- spec/mvc/model.spec.js
+++ spec/mvc/model.spec.js
@@ -4,6 +4,7 @@ describe('Model', function () {
 
 	var TestModel;
 	var attributeObject = {'firstAttribute': 'firstValue', 'secondAttribute': 2};
+	var updatedAttributeValue = 'My updated attribute'
 
 	//spyOn(EventTarget, 'call');
 
@@ -11,11 +12,36 @@ describe('Model', function () {
 
 		beforeEach(function () {
 			TestModel = new MyModel(attributeObject);
-		})
+			spyOn(TestModel, 'fire')
+		});
 
 		it('sets the correct model attributes', function () {
 			expect(TestModel.attributes).toEqual(attributeObject);
-		})
+		});
+
+		describe('Getting a the value of a defined model attribute', function () {
+			
+			it('Gets the correct attribute value', function () {
+				expect(TestModel.get('firstAttribute')).toBe(attributeObject.firstAttribute);
+				expect(TestModel.get('secondAttribute')).toBe(attributeObject.secondAttribute);
+			});
+		});
+
+		describe('Setting a value for a defined model attribute', function () {
+
+			beforeEach(function () {
+				TestModel.set('firstAttribute', updatedAttributeValue);
+			})
+
+			it('Sets the correct attribute value', function () {
+
+				expect(TestModel.attributes['firstAttribute']).toBe(updatedAttributeValue);
+			});
+
+			it('fires the correct custom event', function () {
+				expect(TestModel.fire).toHaveBeenCalledWith('firstAttributeChange');
+			});
+		});
 
 	});
 })
